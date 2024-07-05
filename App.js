8fac34e9791dc12e13323f3a581079e3149ee373
Alexandra Geltzer <geltzer@usf.edu>
2024-07-05 19:45:15 -0400

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import React from 'react';
 import './App.css';
 import PersonalInfo from './PersonalInfo';
 import SkillsList from './SkillsList'; 
-import ExperienceEntry from './ExperienceEntry'; 
+import ExperienceEntry from './ExperienceEntry';
+import EducationBackground from './EducationBackground';
 
 function App() {
   return (
@@ -22,9 +23,22 @@ function App() {
             company="Francesca's"
             duration="November 2023- Present"
           />
-        
       </main>
     </div>
+  <div className="section">
+          <h2>Education</h2>
+          <EducationBackground
+            institution="St. Petersburg College"
+            degree="Associates in Arts"
+            years="2021-2023"
+          />
+          <EducationBackground
+            institution="University of South Florida"
+            degree="Bachelors"
+            years="2023-2025"
+          />
+          {/* Add more EducationBackground components for each educational background */}
+        </div>
   );
 }
 
